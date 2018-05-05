5e07083d388fee1c2020d45de3f1ea240ebef887
ihater <32093379+ihater@users.noreply.github.com>
2018-05-06 00:19:11 +0800

    上传了分页工具类

---
+++ 分页/PageBean.java
@@ -0,0 +1,56 @@
+﻿
+import java.util.List;
+
+/**
+ * 分页显示数据类
+ * @author 传智.郭嘉
+ * 因为我们如果要做分页 的话   ，我们前台要拿到的数据大概有
+ * 	 向页面返回数据:
+ * 	显示商品集合:
+ * 	当前页数:
+ * 	总页数:
+ * 	第 1/12 页
+ * 	通过总记录数.
+ *	 参数每页显示记录数.
+ *	我们要么就是将这些数据手动压入到值栈中，要么就手动提供 get 方法自动压入到值栈，但是我们要的参数太多了
+ *	所以我们就可以提供一个工具类，专门来将这些数据封装到  bean 中去，我们只要在分页那个提供这个工具类  的  get 方法就好了
+ ***** 将数据封装到一个Bean中.
+ */
+public class PageBean<T> {
+	private Integer page;// 当前页数.
+	private Integer limit;// 每页显示记录数
+	private Integer totalCount;// 总记录数
+	private Integer totalPage;// 总页数.
+	private List<T> list; // 显示到浏览器的数据.
+	public Integer getPage() {
+		return page;
+	}
+	public void setPage(Integer page) {
+		this.page = page;
+	}
+	public Integer getLimit() {
+		return limit;
+	}
+	public void setLimit(Integer limit) {
+		this.limit = limit;
+	}
+	public Integer getTotalCount() {
+		return totalCount;
+	}
+	public void setTotalCount(Integer totalCount) {
+		this.totalCount = totalCount;
+	}
+	public Integer getTotalPage() {
+		return totalPage;
+	}
+	public void setTotalPage(Integer totalPage) {
+		this.totalPage = totalPage;
+	}
+	public List<T> getList() {
+		return list;
+	}
+	public void setList(List<T> list) {
+		this.list = list;
+	}
+	
+}
---
+++ 分页/ProductAction.java
@@ -0,0 +1,71 @@
+﻿
+/**
+ * 商品访问的Action类
+ * 
+ * @author 传智.郭嘉
+ * 
+ */
+/**
+ *		 这里我们为什么要继承  ModelDriver  ？？
+ *		因为我们继承了这个，里面传的是 我们的 Product 类，这是 模型驱动！！！！！！！！！！！TM的老是忘记
+ *		这样我们就可以获取到从前台传过来的
+ *		关于商品信息的请求！！！！ 
+ */
+public class ProductAction extends ActionSupport implements
+		ModelDriven<Product> {
+
+	// 接收cid
+	private Integer cid;
+
+	// 接收当前页数
+	private Integer page;
+
+
+	// 注入商品的Service
+	private ProductService productService;
+
+	// 分页的商品显示:
+	private PageBean<Product> pageBean;
+
+
+	/**
+	 *  我们手动提供一个 工具类，让框架去自动封装  我们做分页要 用到的  对象 
+	 *   向页面返回数据:
+	 * 		显示商品集合:
+	 * 		当前页数:
+	 * 		总页数:
+	 * 		第 1/12 页
+	 * 		通过总记录数.
+	 * 		参数每页显示记录数.
+	 ***** 将数据封装到一个Bean中.  
+	 */
+	public PageBean<Product> getPageBean() {			用于 struts2自动封装 page 属性的
+		return pageBean;
+	}
+
+
+	public void setCid(Integer cid) {					前台传值自动拿到 cid
+		this.cid = cid;
+	}
+
+	public Integer getCid() {
+		return cid;
+	}
+
+	public void setPage(Integer page) {
+		this.page = page;
+	}
+
+
+
+
+	// 查询商品的方法:
+	public String findByCid() {
+		
+		// 查询商品:
+		pageBean = productService.findByPage(cid, page);
+
+		return "findByCidSuccess";
+	}
+
+	
